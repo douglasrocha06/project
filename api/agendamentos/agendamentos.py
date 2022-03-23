78410efcf68400add3c8801d06b12d0cab29f1ec
douglas.santos <douglas.santos@boavista>
2022-03-23 13:04:49 -0300

    Adicionando arquivos dockerfile requirements

--- api/agendamentos/agendamentos.py
+++ api/agendamentos/agendamentos.py
@@ -275,4 +275,4 @@ def verificacao(login, senha):
 	return usuarios.get(login) == senha
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
---
+++ api/agendamentos/config.py
@@ -0,0 +1,10 @@
+from app import app
+from flaskext.mysql import MySQL
+import os
+
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
+app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
+app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
+app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+mysql.init_app(app)
